7474a59bb2c7a70949777273b0cdff720b9e3676
Senna Bhany Majid <sennakanjhiwa@gmail.com>
2025-08-12 20:33:42 +0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Nextjs Basic</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
